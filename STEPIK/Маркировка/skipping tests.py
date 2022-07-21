46be7eadcb62929d5907bf71613c6fdf7336aaf5
asiyach <puzach398@gmail.com>
2022-07-21 12:24:15 +0200

    marker

---
+++ STEPIK/Маркировка/skipping tests.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+# Изучите самостоятельно документацию про маркировку xfail. Найдите там параметр, который в случае неожиданного
+# прохождение теста, помеченного как xfail, в отчете пометит этот тест как упавший.
+# Пометьте таким образом первый тест из этого тестового набора:
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
+
+ pytest -s -v test_fixture.py -q --tb=no -p no:warnings^C
